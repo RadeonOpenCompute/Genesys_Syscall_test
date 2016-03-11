37365f88f97c87f0991a1e13eb007689ca7b1e2f
Jan Vesely <jan.vesely@amd.com>
2016-03-10 19:12:32 -0600

    Allocate syscall area
    Signed-off-by: Jan Vesely <jan.vesely@amd.com>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
+#include <iostream>
+#include <hsakmt.h>
+
+enum {
+	WORKITEMS=32
+};
 
 int main(void)
 {
+	void *sc_area = NULL;
+	hsaKmtOpenKFD();
+	HSAKMT_STATUS s = hsaKmtGetSyscallArea(WORKITEMS, &sc_area);
+	::std::cout << "Requested syscall area: " << s << " address: "
+	            << sc_area << ::std::endl;
 	return 0;
 }
