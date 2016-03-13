073211f4ce9fe5c0cd12047d4a3de99d6319a9ea
Jan Vesely <jan.vesely@amd.com>
2016-03-12 18:38:44 -0600

    request sc area big enough for all WIs
    Signed-off-by: Jan Vesely <jan.vesely@amd.com>

--- main.cpp
+++ main.cpp
@@ -7,15 +7,10 @@
 
 #include "amp_syscalls.h"
 
-enum {
-	WORKITEMS=64
-};
-
 #define ARRAY_SIZE(x)  (sizeof(x) / sizeof(x[0]))
 
 int main(int argc, char **argv)
 {
-	syscalls::get().init(WORKITEMS);
 
 	/* for some reason we need a reference,
 	 * function scope globals are probably broken */
@@ -27,6 +22,7 @@ int main(int argc, char **argv)
 	if (argc > 1)
 		parallel = ::std::stoi(argv[1]);
 
+	syscalls::get().init(parallel);
 	for (int i = 0; i < parallel; ++i) {
 		const ::std::string &s = hello[i % ARRAY_SIZE(hello)];
 		::std::cout << "Testing write syscall, the args should be: "
