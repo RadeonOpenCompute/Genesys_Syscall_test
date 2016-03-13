9ffbb5319216190c5d04462b5e55bf198fb6a1fb
Jan Vesely <jan.vesely@amd.com>
2016-03-12 18:30:30 -0600

    allow more parallelism
    Signed-off-by: Jan Vesely <jan.vesely@amd.com>

--- main.cpp
+++ main.cpp
@@ -11,27 +11,37 @@ enum {
 	WORKITEMS=64
 };
 
-int main(void)
+#define ARRAY_SIZE(x)  (sizeof(x) / sizeof(x[0]))
+
+int main(int argc, char **argv)
 {
 	syscalls::get().init(WORKITEMS);
 
 	/* for some reason we need a reference,
 	 * function scope globals are probably broken */
 	syscalls &local = syscalls::get();
-	::std::string hello = "Hello world from GPU!\n";
-	const uint64_t text = (uint64_t)hello.c_str();
-	const size_t length = hello.size();
+	::std::string hello[] = {"Hello world from GPU!\n", "Goodbye!\n"};
+	uint64_t fds[] = {1,2};
 
-	::std::cout << "Testing write syscall, the args should be: "
-	            << __NR_write << ", " << (void*)text << ", " << length
-	            << ::std::endl;
+	int parallel = ARRAY_SIZE(hello);
+	if (argc > 1)
+		parallel = ::std::stoi(argv[1]);
 
-	/* show that we care outside of kernel to prevent DCE */
-	int ret;
-	parallel_for_each(concurrency::extent<1>(1),
+	for (int i = 0; i < parallel; ++i) {
+		const ::std::string &s = hello[i % ARRAY_SIZE(hello)];
+		::std::cout << "Testing write syscall, the args should be: "
+		            << __NR_write << ", " << fds[i % ARRAY_SIZE(fds)]
+		            << ", " << (void*)s.c_str() << ", "
+		            << s.size() << ::std::endl;
+	}
+
+	parallel_for_each(concurrency::extent<1>(parallel),
 	                  [&](concurrency::index<1> idx) restrict(amp)
 	{
-		ret = local.send_nonblock(__NR_write, {1, text, length});
+		int i = idx[0];
+		const ::std::string &s = hello[i % ARRAY_SIZE(hello)];
+		local.send_nonblock(__NR_write, {fds[i % ARRAY_SIZE(fds)],
+		                          (uint64_t)s.c_str(), s.size()});
 	});
 	pid_t p = getpid();
 	::std::cout << "My pid is " << p << " Press any key to continue...\n";
