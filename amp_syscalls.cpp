b5555cbbd8a82c963f93385d1c02847a48c05ed6
Jan Vesely <jan.vesely@amd.com>
2016-05-08 16:18:54 -0500

    amp_syscalls: Use __hsail_get_lane_id()
    combine with get_slot()
    
    Signed-off-by: Jan Vesely <jan.vesely@amd.com>

--- amp_syscalls.cpp
+++ amp_syscalls.cpp
@@ -35,16 +35,15 @@ extern "C" void __hsa_sendmsg(uint32_t msg)restrict(amp);
 //Halt version is not ready yet
 extern "C" void __hsail_barrier(void)restrict(amp);
 
+extern "C" uint32_t __hsail_get_lane_id(void)restrict(amp);
 extern "C" uint32_t __hsa_gethwid(void)restrict(amp);
 
-static unsigned get_slot_page() restrict(amp)
-{
-	uint32_t id = __hsa_gethwid();
-	return (((id >> 4) & 0x3) | ((id >> 6) & 0x1fc)) * 10 + (id & 0xf);
-}
 kfd_sc &syscalls::get_slot() restrict(amp)
 {
-	int idx = (get_slot_page() * 64) + (amp_get_global_id(0) % 64);
+	uint32_t id = __hsa_gethwid();
+	unsigned slot =
+		(((id >> 4) & 0x3) | ((id >> 6) & 0x1fc)) * 10 + (id & 0xf);
+	int idx = (slot * 64) + __hsail_get_lane_id();
 	return syscalls_[idx % elements_];
 }
 
