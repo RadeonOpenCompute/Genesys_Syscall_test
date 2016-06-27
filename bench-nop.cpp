d4b7fb331d06d8a2d7dd319e32b5c7882e2cbf6f
Jan Vesely <jan.vesely@amd.com>
2016-06-26 19:06:22 -0500

    bench-nop: provide separate sources for each variant
    This prevents compiler going crazy with divergent code
    
    Signed-off-by: Jan Vesely <jan.vesely@amd.com>

--- bench-nop.cpp
+++ bench-nop.cpp
@@ -15,25 +15,63 @@ static int run(const test_params &p, ::std::ostream &O,
 	if (p.cpu) {
 		::std::cerr << "Running NOP on CPU is not supported\n";
 	}
-	::std::vector<int> ret(p.parallel);
+	// Initialize with non-zero dummy value
+	::std::vector<int> ret(p.parallel, -7);
 
-	auto start = ::std::chrono::high_resolution_clock::now();
-	parallel_for_each(concurrency::extent<1>(p.parallel),
-	                  [&](concurrency::index<1> idx) restrict(amp)
-	{
-		int i = idx[0];
-		for (size_t j = 0; j < p.serial; ++j) {
-			if (p.gpu_sync_before)
+
+	auto f =
+		[&](concurrency::index<1> idx) restrict(amp) {
+			int i = idx[0];
+			for (size_t j = 0; j < p.serial; ++j)
+				ret[i] = sc.send(0);
+		};
+	auto f_s =
+		[&](concurrency::index<1> idx) restrict(amp) {
+			int i = idx[0];
+			for (size_t j = 0; j < p.serial; ++j) {
 				sc.wait_all();
-			if (p.non_block) {
-				do {
-					ret[i] = sc.send_nonblock(0);
-				} while (ret[i] == EAGAIN);
-			} else {
 				ret[i] = sc.send(0);
 			}
-		}
-	});
+		};
+	auto f_n =
+		[&](concurrency::index<1> idx) restrict(amp) {
+			int i = idx[0];
+			for (size_t j = 0; j < p.serial; ++j) {
+				do {
+				ret[i] = sc.send_nonblock(0);
+				} while (ret[i] == EAGAIN);
+			}
+		};
+	auto f_w_n =
+		[&](concurrency::index<1> idx) restrict(amp) {
+			int i = idx[0];
+			for (size_t j = 0; j < p.serial; ++j) {
+				sc.wait_one_free();
+				ret[i] = sc.send_nonblock(0);
+			}
+		};
+	auto f_s_n =
+		[&](concurrency::index<1> idx) restrict(amp) {
+			int i = idx[0];
+			for (size_t j = 0; j < p.serial; ++j) {
+				sc.wait_all();
+				ret[i] = sc.send_nonblock(0);
+			}
+		};
+	auto start = ::std::chrono::high_resolution_clock::now();
+	if (!p.non_block) {
+		if (!p.gpu_sync_before)
+			parallel_for_each(concurrency::extent<1>(p.parallel), f);
+		else
+			parallel_for_each(concurrency::extent<1>(p.parallel), f_s);
+	} else {
+		if (p.gpu_sync_before)
+			parallel_for_each(concurrency::extent<1>(p.parallel), f_s);
+		else if (p.gpu_wait_before)
+			parallel_for_each(concurrency::extent<1>(p.parallel), f_w_n);
+		else
+			parallel_for_each(concurrency::extent<1>(p.parallel), f_n);
+	}
 	if (p.non_block && !p.dont_wait_after)
 		sc.wait_all();
 	auto end = ::std::chrono::high_resolution_clock::now();
